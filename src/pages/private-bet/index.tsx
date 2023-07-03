663f30a1cf5480deb157bcdacb55bc8d0c4f4914
Clément 'birdy' Danjou <clement@danjou.io>
2023-07-03 15:06:58 +0200

    add the possibility to autoredirect with contract in qs

--- src/pages/private-bet/index.tsx
+++ src/pages/private-bet/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { HeadFC, PageProps } from 'gatsby';
+import { ContractFactory, isAddress } from 'ethers';
+import queryString from 'query-string';
 import { Contract } from '../../components/Contract';
 import { Connect } from '../../components/Connect';
 import { PrivateBet } from '../../modules/private-bet';
@@ -9,9 +11,19 @@ import privateBet from '../../contracts/PrivateBet.json';
 import encryptedERC20 from '../../contracts/EncryptedERC20.json';
 
 import '../default.css';
-import { ContractFactory } from 'ethers';
 
 const IndexPage: React.FC<PageProps> = withTheme(() => {
+  if (location.search) {
+    const params = queryString.parse(location.search);
+    if (params.contract) {
+      if (isAddress(params.contract)) {
+        localStorage.setItem('privateBet', params.contract);
+      }
+      if (typeof window !== 'undefined') {
+        (window.location as any) = location.pathname;
+      }
+    }
+  }
   return (
     <main className="Main">
       <Connect back title="Double or nothing">
@@ -52,4 +64,4 @@ const IndexPage: React.FC<PageProps> = withTheme(() => {
 
 export default IndexPage;
 
-export const Head: HeadFC = () => <title>Zama Blind Auction</title>;
+export const Head: HeadFC = () => <title>Zama Private Bet</title>;
