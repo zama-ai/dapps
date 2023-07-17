296e95eee0fa0054353061c0fca4a36935f8eb2b
Clément 'birdy' Danjou <clement@danjou.io>
2023-07-17 16:20:11 +0200

    feat() update explorer default url

--- src/components/Connect/Connect.tsx
+++ src/components/Connect/Connect.tsx
@@ -118,7 +118,7 @@ export const Connect: React.FC<{
               symbol: 'ZAMA',
               decimals: 18,
             },
-            blockExplorerUrls: ['https://explorer.zama.ai'],
+            blockExplorerUrls: ['https://main.explorer.zama.ai'],
           },
         ],
       });
