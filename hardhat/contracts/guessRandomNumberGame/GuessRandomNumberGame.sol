f16da4c06b8c777774faa915fa9cbe22e623c2ba
Aurora Poppyseed <30662672+poppyseedDev@users.noreply.github.com>
2025-01-13 17:39:29 +0100

    added readme

--- hardhat/contracts/guessRandomNumberGame/GuessRandomNumberGame.sol
+++ hardhat/contracts/guessRandomNumberGame/GuessRandomNumberGame.sol
@@ -11,7 +11,7 @@ import "fhevm/gateway/GatewayCaller.sol";
 /// @notice A decentralized game where players submit their encrypted guesses to guess the random number
 contract GuessRandomNumberGame is SepoliaZamaFHEVMConfig, SepoliaZamaGatewayConfig, GatewayCaller {
     // uint16 is best suited for this task since -> uint16: 0 to 65,535 (2^16 - 1)
-    uint16 public constant MAX_VAlUE = 1024; // Upper bound must be a power of 2
+    uint16 public immutable MAX_VALUE;
     uint8 public immutable MIN_PLAYERS;
     euint16 encryptedTarget;
     uint16 public decryptedPreviousTarget; // Decrypted previous encrypted target
@@ -23,14 +23,14 @@ contract GuessRandomNumberGame is SepoliaZamaFHEVMConfig, SepoliaZamaGatewayConf
     // Adding player scores to make it more fun
     mapping(address => uint256) public playerScores;
 
-    struct Secret {
+    struct Guess {
         euint16 encryptedValue;
         address owner;
     }
-    Secret[] public secrets;
+    Guess[] public guesses;
 
     /// @notice Event emitted when a secret is submitted
-    event SecretSubmitted(address indexed player, euint16 encryptedValue);
+    event GuessSubmitted(address indexed player, euint16 encryptedValue);
 
     /// @notice Event emitted when a winner is determined
     event WinnerDeclared(address indexed winner, uint16 winningValue);
@@ -39,9 +39,10 @@ contract GuessRandomNumberGame is SepoliaZamaFHEVMConfig, SepoliaZamaGatewayConf
     event GameReset();
 
     /// @notice Constructor sets a random encrypted target value
-    constructor(uint8 _minPlayers) {
+    constructor(uint8 _minPlayers, uint16 _max_value) {
         MIN_PLAYERS = _minPlayers;
-        encryptedTarget = TFHE.randEuint16(MAX_VAlUE);
+        MAX_VALUE = _max_value; // Upper bound must be a power of 2
+        encryptedTarget = TFHE.randEuint16(MAX_VALUE);
         // Encrypt and store the target value
         TFHE.allowThis(encryptedTarget);
     }
@@ -51,45 +52,45 @@ contract GuessRandomNumberGame is SepoliaZamaFHEVMConfig, SepoliaZamaGatewayConf
     /// @param inputProof The ZK proof for the secret
     function submitGuess(einput secretValue, bytes calldata inputProof) public {
         // Check if address has already participated
-        for (uint i = 0; i < secrets.length; i++) {
-            require(secrets[i].owner != msg.sender, "Address has already participated");
+        for (uint i = 0; i < guesses.length; i++) {
+            require(guesses[i].owner != msg.sender, "Address has already participated");
         }
 
-        euint16 validatedSecret = TFHE.asEuint16(secretValue, inputProof);
+        euint16 validatedGuess = TFHE.asEuint16(secretValue, inputProof);
         // allowing the contract and the user to access this contract later
-        TFHE.allowThis(validatedSecret);
-        TFHE.allow(validatedSecret, msg.sender);
+        TFHE.allowThis(validatedGuess);
+        TFHE.allow(validatedGuess, msg.sender);
 
         // Store the secret
-        secrets.push(Secret({ encryptedValue: validatedSecret, owner: msg.sender }));
+        guesses.push(Guess({ encryptedValue: validatedGuess, owner: msg.sender }));
 
-        emit SecretSubmitted(msg.sender, validatedSecret);
+        emit GuessSubmitted(msg.sender, validatedGuess);
     }
 
     /// @notice Get the encrypted secret value for the caller
     /// @return The encrypted secret value for the caller
-    function getMySecret() public view returns (euint16) {
-        for (uint i = 0; i < secrets.length; i++) {
-            if (secrets[i].owner == msg.sender) {
-                return secrets[i].encryptedValue;
+    function getMyGuess() public view returns (euint16) {
+        for (uint i = 0; i < guesses.length; i++) {
+            if (guesses[i].owner == msg.sender) {
+                return guesses[i].encryptedValue;
             }
         }
         revert("No secret found for caller");
     }
 
-    /// @notice Compare all secrets to the encrypted target and determine the winner
+    /// @notice Compare all guesses to the encrypted target and determine the winner
     function determineWinner() public {
-        require(secrets.length >= MIN_PLAYERS, "Not enough players have submitted guesses");
+        require(guesses.length >= MIN_PLAYERS, "Not enough players have submitted guesses");
 
         closestValue = TFHE.asEuint16(0); // Start with 0 as the closest value
-        euint16 smallestDifference = TFHE.asEuint16(MAX_VAlUE); // Start with max value
+        euint16 smallestDifference = TFHE.asEuint16(MAX_VALUE); // Start with max value
         closestOwner = TFHE.asEaddress(address(this)); // Begin closest owner with this address
 
-        for (uint i = 0; i < secrets.length; i++) {
+        for (uint i = 0; i < guesses.length; i++) {
             euint16 difference = TFHE.select(
-                TFHE.gt(secrets[i].encryptedValue, encryptedTarget),
-                TFHE.sub(secrets[i].encryptedValue, encryptedTarget),
-                TFHE.sub(encryptedTarget, secrets[i].encryptedValue)
+                TFHE.gt(guesses[i].encryptedValue, encryptedTarget),
+                TFHE.sub(guesses[i].encryptedValue, encryptedTarget),
+                TFHE.sub(encryptedTarget, guesses[i].encryptedValue)
             );
             ebool diff_smaller = TFHE.lt(difference, smallestDifference);
 
@@ -97,10 +98,10 @@ contract GuessRandomNumberGame is SepoliaZamaFHEVMConfig, SepoliaZamaGatewayConf
             smallestDifference = TFHE.select(diff_smaller, difference, smallestDifference);
 
             // Update the closest value and owner if the difference is smaller
-            closestValue = TFHE.select(diff_smaller, secrets[i].encryptedValue, closestValue);
+            closestValue = TFHE.select(diff_smaller, guesses[i].encryptedValue, closestValue);
 
             // Update the closest owner
-            closestOwner = TFHE.select(diff_smaller, TFHE.asEaddress(secrets[i].owner), closestOwner);
+            closestOwner = TFHE.select(diff_smaller, TFHE.asEaddress(guesses[i].owner), closestOwner);
         }
 
         requestDecrypt();
@@ -137,14 +138,14 @@ contract GuessRandomNumberGame is SepoliaZamaFHEVMConfig, SepoliaZamaGatewayConf
 
     /// @notice Reset the game state for a new round
     function resetGame() internal {
-        // Reset the secrets array
-        delete secrets;
+        // Reset the guesses array
+        delete guesses;
 
         // Reset encrypted values
         closestValue = TFHE.asEuint16(0);
         closestOwner = TFHE.asEaddress(address(0));
 
-        encryptedTarget = TFHE.randEuint16(MAX_VAlUE);
+        encryptedTarget = TFHE.randEuint16(MAX_VALUE);
 
         emit GameReset();
     }
