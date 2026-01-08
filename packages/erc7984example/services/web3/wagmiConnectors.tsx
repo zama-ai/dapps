80d07235f7166488b5477f58327c0013027d3b28
poppyseeddev <aurora.makovac@gmail.com>
2026-01-08 14:57:23 +0100

    removed coinbase sdk because multithreading issues

--- packages/erc7984example/services/web3/wagmiConnectors.tsx
+++ packages/erc7984example/services/web3/wagmiConnectors.tsx
@@ -1,6 +1,5 @@
 import { connectorsForWallets } from "@rainbow-me/rainbowkit";
 import {
-  coinbaseWallet,
   ledgerWallet,
   metaMaskWallet,
   rainbowWallet,
@@ -17,7 +16,6 @@ const wallets = [
   metaMaskWallet,
   walletConnectWallet,
   ledgerWallet,
-  coinbaseWallet,
   rainbowWallet,
   safeWallet,
   ...(!targetNetworks.some(network => network.id !== (chains.hardhat as chains.Chain).id) || !onlyLocalBurnerWallet
